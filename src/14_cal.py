e0973a2330e201738289c979ece037fba1d75720
unknown <jacob.e.england@gmail.com>
2020-07-06 11:31:54 -0400

    Added range checking to cal.py; enforced pep8 on cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,13 +18,14 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
-optional, as this is a common convention in documentation.
+Note: the user should provide argument input (in the initial call to run the
+file) and not prompted input. Also, the brackets around year are to denote
+that the argument is optional, as this is a common convention in
+documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
-it should use today’s date to get the month and year.
+This would mean that from the command line you would call `python3 14_cal.py
+4 2015` to print out a calendar for April in 2015, but if you omit either the
+year or both values, it should use today’s date to get the month and year.
 """
 
 import sys
@@ -36,11 +37,20 @@ year = datetime.now().year
 
 if(len(sys.argv) == 3):
     month = int(sys.argv[1])
-    year = int(sys.argv[2])  
+    if(int(sys.argv[1]) <= 12 and int(sys.argv[1]) >= 1):
+        month = int(sys.argv[1])
+    else:
+        print('Invalid month; please input an integer between 1 and 12')
+        exit()
+    year = int(sys.argv[2])
 elif(len(sys.argv) == 2):
-    month = int(sys.argv[1])
+    if(int(sys.argv[1]) <= 12 and int(sys.argv[1]) >= 1):
+        month = int(sys.argv[1])
+    else:
+        print('Invalid month; please input an integer between 1 and 12')
+        exit()
 elif(len(sys.argv) > 3):
-    print('Arguments should be as follows: month, year')
+    print("Arguments should be as follows: month, year")
     exit()
 
 y = calendar.month(year, month)
